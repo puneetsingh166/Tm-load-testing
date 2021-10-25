b0e9a74c3d2ef97b161517176ff0edd38e1a23cc
Puneet <puneet@payatu.io>
2021-10-25 11:51:25 +0530

    cleaned code

--- client/docs/statik/init.go
+++
@@ -1,3 +0,0 @@
-package statik
-
-// This just for fixing the error in importing empty github.com/puneetsingh166/tm-load-test/client/docs/statik
--- cosmovisor/cmd/cosmovisor/cmd/root.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-// RunCosmovisorCommands executes cosmosvisor commands e.g `cosmovisor version`
-func RunCosmovisorCommands(args []string) {
-	if isVersionCommand(args) {
-		printVersion()
-	}
-}
